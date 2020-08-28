9145dc87499a64ddc8bb7480295ab598b34364e6
misschildhood <923219711@qq.com>
2020-08-28 09:01:57 +0800

    报表统计分析

--- src/main/scala/com/itcast/dmp/tags/tagRunner.scala
+++ src/main/scala/com/itcast/dmp/tags/tagRunner.scala
@@ -3,7 +3,9 @@ package com.itcast.dmp.tags
 import ch.hsr.geohash.GeoHash
 import cn.itcast.etl.ETLRunner
 import com.itcast.dmp.area.BusinessAreaRunner
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+import scala.collection.mutable
 
 object tagRunner {
 
@@ -26,21 +28,46 @@ object tagRunner {
     }
 
     val ods = odsOption.get
-    val area=areaOption.get
+    val area = areaOption.get
 
     import spark.implicits._
     import org.apache.spark.sql.functions._
 
 
     val geoHash = udf(toGeoHash _)
-    val odsWithGeoHash=ods.withColumn("geoHash", geoHash('longitude,'latitude))
+    val odsWithGeoHash = ods.withColumn("geoHash", geoHash('longitude, 'latitude))
     val odsWithArea: DataFrame = odsWithGeoHash.join(
       area,
       odsWithGeoHash.col("geoHash") === area.col("geoHash"),
       "left"
     )
+    odsWithArea.map(createTags)
 
+  }
 
+  def createTags(row: Row):IdsWithTags = {
+    // 3.4 生成标签数据
+    val tags = mutable.Map[String, Int]()
+    // 3.4.1 广告标识
+    tags += ("AD" + row.getAs[Long]("adspacetype") -> 1)
+    // 3.4.2 渠道信息
+    tags += ("CH" + row.getAs[String]("channelid") -> 1)
+    // 3.4.3 关键词 keywords -> 帅哥,有钱
+    row.getAs[String]("keywords").split(",")
+      .map("KW" + _ -> 1)
+      .foreach(tags += _)
+    // 3.4.4 省市标签
+    tags += ("PN" + row.getAs[String]("region") -> 1)
+    tags += ("CN" + row.getAs[String]("city") -> 1)
+    // 3.4.5 性别标签
+    tags += ("GD" + row.getAs[String]("sex") -> 1)
+    // 3.4.6 年龄标签
+    tags += ("AG" + row.getAs[String]("age") -> 1)
+    // 3.4.7 商圈标签, 正常情况下, 在这里需要生成 GeoHash, 然后再查询 Kudu 表, 获取数据
+    // 优化写法, 直接先进行 join, 把商圈信息添加进来, 然后直接取
+    row.getAs[String]("area").split(",")
+      .map("A" + _ -> 1)
+      .foreach(tags += _)
   }
 
   def toGeoHash(longitude: Double, latitude: Double) = {
@@ -51,3 +78,4 @@ object tagRunner {
   private val AREA_TABLE = BusinessAreaRunner.AREA_TABLE_NAME
 
 }
+case class IdsWithTags(mainId: String, ids: Map[String, String], tags: Map[String, Int])
